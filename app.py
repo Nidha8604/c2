2da56a2e35ccd840d4a4f035b6156b4d7f8886b7
Nidha8604 <fathima.nidha2022@vitstudent.ac.in>
2025-04-24 19:57:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,21 +10,19 @@ st.title("🌿 University Green Space & Wellbeing Predictor")
 df = pd.read_csv("university_student_wellbeing_synthetic.csv")
 
 # Define features and target
-features = ["NDVI Score", "Green Space Area (sq.meters)",
-            "Walking Distance (mins)", "Shade Coverage (%)", "Academic Stress Level"]
+features = ["NDVI Score","Walking Distance (mins)", "Shade Coverage (%)", "Academic Stress Level"]
 target = "Predicted Wellbeing Score"
 
 # Sidebar - Input Parameters
 st.sidebar.header("Input Green Space and Student Parameters")
 
 ndvi = st.sidebar.slider("NDVI Score", 0.3, 0.9, 0.6)
-area = st.sidebar.number_input("Green Space Area (sq. meters)", 100, 10000, 5000)
 walk = st.sidebar.slider("Walking Distance (mins)", 1, 30, 10)
 shade = st.sidebar.slider("Shade Coverage (%)", 0.0, 100.0, 50.0)
 stress = st.sidebar.slider("Academic Stress Level", 1, 10, 5)
 
 # Prepare input
-input_df = pd.DataFrame([[ndvi, area, walk, shade, stress]], columns=features)
+input_df = pd.DataFrame([[ndvi, walk, shade, stress]], columns=features)
 
 # Train model
 X = df[features]
